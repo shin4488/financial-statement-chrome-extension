082ba308174c32a63a0df918990d6689af6a918b
Shin <37034832+shin4488@users.noreply.github.com>
2024-10-14 21:12:22 +0900

    Change logo images (#2)
    * change: logo images
    
    * change: apply changing of logo images to manifest
    
    * change: apply url changed in the same tab
    
    * fix: url null handling

--- src/background/index.ts
+++ src/background/index.ts
@@ -9,9 +9,14 @@ store.subscribe(() => {
   // console.log('state', state);
 });
 
-browser.tabs.onActivated.addListener(async () => {
+const changeStateByActivatedTag = async () => {
   const activeTabs = await browser.tabs.query({ active: true, currentWindow: true });
-  if (activeTabs.length === 0 || activeTabs[0].url === undefined) {
+  if (
+    activeTabs.length === 0 ||
+    activeTabs[0].url === undefined ||
+    activeTabs[0].url === null ||
+    activeTabs[0].url === ''
+  ) {
     return;
   }
 
@@ -23,4 +28,10 @@ browser.tabs.onActivated.addListener(async () => {
   } else {
     browser.action.disable();
   }
-});
+};
+
+// タブ切り替えのため
+browser.tabs.onActivated.addListener(changeStateByActivatedTag);
+// 新規タブを開いたり、URLバーからサイト移動した時のため
+browser.tabs.onUpdated.addListener(changeStateByActivatedTag);
+browser.windows.onFocusChanged.addListener(changeStateByActivatedTag);

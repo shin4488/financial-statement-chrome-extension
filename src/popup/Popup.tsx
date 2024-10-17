2d2ccc40b1a6d2c66b15b5b09b3195a5f783d2f5
shin4488 <efshinya48@gmail.com>
2024-10-17 19:23:45 +0900

    add: send logEvent about stock code & site domain to google analytics & add environmental variables for analytics

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,7 +1,13 @@
 import FinancialStatementList from '@/app/features/financialStatementList/FinancialStatementList';
 import DefaultLayout from '@/app/layouts/default/DefaultLayout';
+import { logLoadStatementsEventToAnalytics } from '@/app/plugins/google/analytics';
+import store from '@/app/store';
 
 const Popup = () => {
+  // 開いた時点のページに関する情報をGoogleアナリティクスへ送信
+  const sitePage = store.getState().sitePage;
+  logLoadStatementsEventToAnalytics(sitePage.siteDomain, sitePage.stockCode);
+
   // ポップアップ画面の大きさの定義
   document.body.className = 'w-[31rem] h-[15rem]';
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,45 @@
 import { crx } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import manifest from './src/manifest';
 
-export default defineConfig({
-  // @see https://github.com/crxjs/chrome-extension-tools/issues/696
-  server: {
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      port: 5173,
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, process.cwd());
+
+  return {
+    define: {
+      VITE_GOOGLE_ANALYTICS_API_KEY: JSON.stringify(env.VITE_GOOGLE_ANALYTICS_API_KEY),
+      VITE_GOOGLE_MEASUREMENT_ID: JSON.stringify(env.VITE_GOOGLE_MEASUREMENT_ID),
+      VITE_GOOGLE_FIREBASE_APP_ID: JSON.stringify(env.VITE_GOOGLE_FIREBASE_APP_ID),
     },
-    // https://github.com/vitejs/vite/discussions/3396#discussioncomment-10371388
-    // vite --hostで起動する
-    host: true,
-  },
-  // prevent src/ prefix on extension urls
-  root: resolve(__dirname, 'src'),
-  publicDir: resolve(__dirname, 'public'),
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'assets/chunk-[hash].js',
+    // @see https://github.com/crxjs/chrome-extension-tools/issues/696
+    server: {
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        port: 5173,
+      },
+      // https://github.com/vitejs/vite/discussions/3396#discussioncomment-10371388
+      // vite --hostで起動する
+      host: true,
+    },
+    // prevent src/ prefix on extension urls
+    root: resolve(__dirname, 'src'),
+    publicDir: resolve(__dirname, 'public'),
+    build: {
+      outDir: resolve(__dirname, 'dist'),
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/chunk-[hash].js',
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
     },
-  },
-  plugins: [react(), crx({ manifest })],
+    plugins: [react(), crx({ manifest })],
+  };
 });
